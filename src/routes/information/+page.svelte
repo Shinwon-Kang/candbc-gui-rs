b22bb129671c31d765a42eeb935df5aa0a3fff00
kangshinwon <wnfansl788@gmail.com>
2024-08-09 17:03:51 +0900

    change tabs style

--- src/routes/information/+page.svelte
+++ src/routes/information/+page.svelte
@@ -9,8 +9,8 @@
 	import { Mail, Info } from 'lucide-svelte';
 
 	let tabs = [
-		{ name: 'Information', icon: Info, comp: EmptyComp },
-		{ name: 'Messages', icon: Mail, comp: EmptyComp }
+		{ name: 'Information', icon: Info, comp: InfoView },
+		{ name: 'Messages', icon: Mail, comp: MessageView }
 	];
 
 	let cur = tabs[0];
@@ -18,7 +18,11 @@
 
 <span class="">
 	{#each tabs as tab}
-		<button class:selected={cur === tab} on:click={() => (cur = tab)}>
+		<button
+			class:selected={cur === tab}
+			on:click={() => (cur = tab)}
+			class="relative p-2 px-4 border -mb-px -mt-px -ml-px border-b"
+		>
 			<span class="flex gap-x-2 mx-2 my-1">
 				<svelte:component this={tab.icon} />
 				{tab.name}
@@ -27,48 +31,20 @@
 	{/each}
 </span>
 
-<main class="bg-orange-300 h-full m-5">
+<main class="h-full overflow-y-auto border-t p-5">
 	<svelte:component this={cur.comp} />
 </main>
 
-<!-- <div class="bg-green-300">
-	<Tabs.Root value="info" class="">
-		<Tabs.List class="w-full grid grid-cols-2">
-			<Tabs.Trigger value="info">Info</Tabs.Trigger>
-			<Tabs.Trigger value="message">Message</Tabs.Trigger>
-		</Tabs.List>
-		<Tabs.Content value="info">
-			<InfoView />
-		</Tabs.Content>
-		<Tabs.Content value="message">
-			<div class="mt-10">
-				<MessageView />
-			</div>
-		</Tabs.Content>
-	</Tabs.Root>
-</div> -->
-
 <style>
-	button {
-		cursor: pointer;
-		padding: 0.5rem 1rem;
-		border: 1px solid transparent;
-		position: relative;
-		margin-bottom: -1px;
-		margin-top: -1px;
-		margin-left: -1px;
-		background-color: transparent;
-		border-bottom: 1px solid hsl(214.3 31.8% 91.4%);
-	}
-	button.selected {
-		border-right: 1px solid hsl(214.3 31.8% 91.4%);
-		border-left: 1px solid hsl(214.3 31.8% 91.4%);
-		border-bottom: 1px solid #fff;
+	:global(:root) {
+		--color: hsla(0, 0%, 100%, 1);
 	}
 
-	main {
-		font-size: 1rem;
-		padding: 2rem;
-		border-top: 1px solid hsl(214.3 31.8% 91.4%);
+	:global(:root.dark) {
+		--color: hsla(222.2, 84%, 4.9%, 1);
+	}
+
+	button.selected {
+		border-bottom: 1px solid var(--color);
 	}
 </style>
